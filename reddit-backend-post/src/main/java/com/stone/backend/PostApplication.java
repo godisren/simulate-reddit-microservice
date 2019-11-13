b8d323ed4d6c317b4b4c5ba22e44cee2441af181
godisren <godisren@gmail.com>
2019-11-13 15:31:43 +0800

    rename AccountApplication.java as PostApplication.java

--- reddit-backend-post/src/main/java/com/stone/backend/PostApplication.java
+++ reddit-backend-post/src/main/java/com/stone/backend/PostApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AccountApplication {
+public class PostApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
+		SpringApplication.run(PostApplication.class, args);
 	}
 
 }
